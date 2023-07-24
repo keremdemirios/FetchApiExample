4d01936d65ce9175c4922ad58c6fd6a544b29184
Kerem Demir <keremdemir@192.168.1.112>
2023-07-24 16:10:22 +0300

    Fetch API MVVM iCrypto.

---
+++ FetchAPI/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FetchAPI
+//
+//  Created by Kerem Demir on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
